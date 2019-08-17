6a89a66196db7e01e365a4a01a23dd0c0d53f04a
Vaibhav_Mishra_IIT_Jodhpur <mishra.4@iitj.ac.in>
2019-08-17 12:59:24 +0530

    Update ARDUINO_IR_SENSOR.ino

--- sensors/ARDUINO_IR_SENSOR.ino
+++ sensors/ARDUINO_IR_SENSOR.ino
@@ -1,1 +1,19 @@
 
+// IR SENSOR 
+
+
+int potPin = A2;    // select the input pin for the potentiometer
+int ledPin = 13;   // select the pin for the LED
+int val = 0;       // variable to store the value coming from the sensor
+
+void setup() {
+  Serial.begin(9600);
+    // declare the ledPin as an OUTPUT
+}
+
+void loop() {
+  val = analogRead(potPin);    // read the value from the sensor
+ 
+  Serial.println(val);
+  delay(100);
+}
