5885af87510c0f2f31b0dcf471783d7928255c96
Vaibhav_Mishra_IIT_Jodhpur <mishra.4@iitj.ac.in>
2019-08-17 12:32:57 +0530

    Update ARDUINO_KEYPAD.ino

--- sensors/ARDUINO_KEYPAD.ino
+++ sensors/ARDUINO_KEYPAD.ino
@@ -1,1 +1,33 @@
 
+// https://www.instructables.com/id/Connecting-a-4-x-4-Membrane-Keypad-to-an-Arduino/
+
+
+
+
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+   Serial.begin(9600);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    Serial.println(key);
+  }
+}
